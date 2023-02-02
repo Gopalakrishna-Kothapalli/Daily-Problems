982c2528b54dcf1410ec53dc3a6373737ffdbefe
Gopalakrishna-Kothapalli <89624550+Gopalakrishna-Kothapalli@users.noreply.github.com>
2023-02-02 09:28:24 +0530

    Create merge_soreted_array.java

---
+++ 01Feb2023/merge_soreted_array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+         int j=0;
+           for(int i=m;i<m+n;i++){
+           nums1[i] = nums2[j++];    
+           }
+          Arrays.sort(nums1);      
+   
+    }
+}
