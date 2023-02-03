1bb38b4220243055e5e4f159a85959945e9376da
Gopalakrishna-Kothapalli <89624550+Gopalakrishna-Kothapalli@users.noreply.github.com>
2023-02-03 09:49:54 +0530

    Create pangrams.java

---
+++ one_week_prep/pangrams.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String pangrams(String s) {
+    // Write your code here
+     s=s.toLowerCase();
+     s=s.replaceAll(" ", "");
+     TreeSet<Character> ts = new TreeSet<>();
+     for(int i=0;i<s.length();i++){
+         ts.add(s.charAt(i));
+     }
+     if(ts.size()==26){
+         return "pangram";
+     }
+     else{
+           return "not pangram";
+     }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
